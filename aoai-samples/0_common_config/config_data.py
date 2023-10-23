9b064f68814043730357be33c4d5e1ac4a6723b0
Srikantan Sankaran <sansri@microsoft.com>
2023-10-23 16:03:40 +0530

    cleansed the config

--- aoai-samples/0_common_config/config_data.py
+++ aoai-samples/0_common_config/config_data.py
@@ -1,7 +1,7 @@
 import openai
 
 def set_environment_details_turbo():
-    openai.api_key = 'f9f6178d2045490eb5dc707d1889c1f6'
+    openai.api_key = 'xxxxxxxxxxxxxxxxxxx'
     openai.api_base =  'https://aoai-975.openai.azure.com/' # your endpoint should look like the following https://YOUR_RESOURCE_NAME.openai.azure.com/
     openai.api_type = 'azure'
     openai.api_version = '2023-07-01-preview'
@@ -9,14 +9,14 @@ def set_environment_details_turbo():
 
 
 def get_environment_details_turbo():
-    return 'f9f6178d2045490eb5dc707d1889c1f6', 'https://aoai-975.openai.azure.com/', 'azure', '2023-07-01-preview'
+    return 'xxxxxxxxxxxxxxxxxxxxx', 'https://aoai-975.openai.azure.com/', 'azure', '2023-07-01-preview'
 
 def get_deployment_name_turbo():
     return 'turbo0613'
 
 
 def set_environment_details_gpt4():
-    openai.api_key = 'e4c6d4474b294e2fa31e1deb2fd298b6'
+    openai.api_key = 'xxxxxxxxxxxxxxxxxx'
     openai.api_base =  'https://oaisvcsansri.openai.azure.com/'
     openai.api_type = 'azure'
     openai.api_version = '2023-07-01-preview'
