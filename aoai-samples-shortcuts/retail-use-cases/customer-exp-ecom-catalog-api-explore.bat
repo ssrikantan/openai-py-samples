468ba2e76e22483b6bf669264471ce4d933b45d7
ssrikantan <sansri@microsoft.com>
2023-11-24 18:56:49 +0530

    added sample prompts

--- aoai-samples-shortcuts/retail-use-cases/customer-exp-ecom-catalog-api-explore.bat
+++ aoai-samples-shortcuts/retail-use-cases/customer-exp-ecom-catalog-api-explore.bat
@@ -1,4 +1,4 @@
 cd "..\..\aoai-samples\retail-use-cases\ecom-api-interaction-app"
-start notepad 2-RentalAgreementTemplate.txt
+start notepad sample-prompts.txt
 start microsoft-edge:https://contoso-ecom.azurewebsites.net/swagger.json
 streamlit run bot-app.py
